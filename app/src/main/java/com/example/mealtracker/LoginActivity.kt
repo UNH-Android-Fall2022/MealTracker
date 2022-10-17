c6548a59b71f51a490088ad1f9a17f0c843ebad2
anvesh-lp <knanvesh.01@gmail.com>
2022-10-17 12:02:53 -0400

    Adding user login functionality using firebase

--- app/src/main/java/com/example/mealtracker/LoginActivity.kt
+++ app/src/main/java/com/example/mealtracker/LoginActivity.kt
@@ -2,16 +2,69 @@ package com.example.mealtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
 import com.example.mealtracker.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private val db = FirebaseAuth.getInstance()
+    private val TAG: String = "In Login Activity"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+//      https://firebase.google.com/docs/auth/android/start#kotlin+ktx_3
+
+
+        binding.loginbtn.setOnClickListener {
+            when {
+                TextUtils.isEmpty(binding.username.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Please enter username/email",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                TextUtils.isEmpty(binding.password.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Please enter password",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else -> {
+                    val emailId: String = binding.username.text.toString().trim { it <= ' ' }
+                    val password: String = binding.password.text.toString().trim { it <= ' ' }
+                    db.signInWithEmailAndPassword(emailId, password)
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                Log.d(TAG, "Sign in successfully")
+                                val user = db.currentUser
+                                Toast.makeText(
+                                    this@LoginActivity,
+                                    "Login Successfully",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            } else {
+                                Log.d(TAG, "Sign in failed", task.exception)
+                                Toast.makeText(
+                                    this@LoginActivity,
+                                    task.exception!!.message.toString(),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                }
+            }
+        }
     }
 }
