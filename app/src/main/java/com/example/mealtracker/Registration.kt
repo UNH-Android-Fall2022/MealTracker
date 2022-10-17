340832dfdb29205dccfbb90799d53b32cffc41c9
anvesh-lp <knanvesh.01@gmail.com>
2022-10-16 20:28:23 -0400

    Adding user registration functionality using firebase

--- app/src/main/java/com/example/mealtracker/Registration.kt
+++ app/src/main/java/com/example/mealtracker/Registration.kt
@@ -1,15 +1,20 @@
 package com.example.mealtracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
 import com.example.mealtracker.databinding.ActivityRegistrationBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 
 class Registration : AppCompatActivity() {
     private lateinit var binding: ActivityRegistrationBinding
+    private val db = FirebaseAuth.getInstance()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,6 +64,32 @@ class Registration : AppCompatActivity() {
 
                     val emailId: String = binding.username.text.toString().trim() { it <= ' ' }
                     val password: String = binding.password.text.toString().trim() { it <= ' ' }
+
+                    db.createUserWithEmailAndPassword(emailId, password).addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            val firebaseUser: FirebaseUser = task.result!!.user!!
+                            Toast.makeText(
+                                this@Registration,
+                                "Created User succesfully",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            val intent = Intent(this@Registration, LoginActivity::class.java)
+                            intent.flags =
+                                Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            intent.putExtra("userId", firebaseUser.uid)
+                            intent.putExtra("email", emailId)
+                            startActivity(intent)
+                            finish()
+                        } else {
+
+                            Toast.makeText(
+                                this@Registration,
+                                "Registration Unsuccefull",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
 
                 }
             }
