af5eab25dc7743fcd62913964466b860d4afcdf6
anvesh-lp <knanvesh.01@gmail.com>
2022-10-19 18:50:10 -0400

    developed Skeleton view of fragment

--- app/src/main/java/com/example/mealtracker/MainActivity.kt
+++ app/src/main/java/com/example/mealtracker/MainActivity.kt
@@ -8,17 +8,29 @@ import android.view.MenuInflater
 import android.widget.Toast
 import com.example.mealtracker.databinding.ActivityLoginBinding
 import com.example.mealtracker.databinding.ActivityRegistrationBinding
+import com.example.mealtracker.fragments.HomeFragment
+import com.github.mikephil.charting.data.PieEntry
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var pieList:ArrayList<PieEntry>;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fragmentManage= supportFragmentManager
+        val fragmenttrans=fragmentManage.beginTransaction()
+        fragmenttrans.replace(R.id.fragment_container,HomeFragment.newInstance("hello","hello2"))
         setContentView(R.layout.activity_main)
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.nav_menu, menu)
+
         return super.onCreateOptionsMenu(menu)
+
     }
+
 }
