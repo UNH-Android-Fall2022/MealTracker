c1a3d9e5cec1b8f50f7e2e78d1f8154399549887
SaiNandan1998 <nandan.ponakala98@gmail.com>
2022-10-21 17:04:45 -0400

    Getting user ID from fire base

--- app/src/main/java/com/example/mealtracker/MainActivity.kt
+++ app/src/main/java/com/example/mealtracker/MainActivity.kt
@@ -1,23 +1,25 @@
 package com.example.mealtracker
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
-import com.example.mealtracker.databinding.FragmentHomeBinding
 import com.example.mealtracker.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
 
-    private lateinit var binding: FragmentHomeBinding
+    private lateinit var USER_ID: String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        USER_ID = intent.getStringExtra("USER_ID").toString()
+        Log.d("onCreate: ", USER_ID)
 
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
