33f2d16dc5262473be3d26d0876456c248daef7f
anvesh-lp <knanvesh.01@gmail.com>
2022-11-13 17:05:56 -0500

    adding bar chart to week fragment

--- app/src/main/java/com/example/mealtracker/fragments/WeekFragment.kt
+++ app/src/main/java/com/example/mealtracker/fragments/WeekFragment.kt
@@ -11,10 +11,9 @@ import androidx.fragment.app.Fragment
 import com.example.mealtracker.R
 import com.example.mealtracker.databinding.FragmentWeekBinding
 import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.PieChart
-import com.github.mikephil.charting.data.PieData
-import com.github.mikephil.charting.data.PieDataSet
-import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.formatter.PercentFormatter
 import com.github.mikephil.charting.utils.MPPointF
 import com.google.firebase.auth.FirebaseAuth
@@ -37,6 +36,10 @@ class WeekFragment : Fragment() {
     private lateinit var authenticaion: FirebaseAuth
     lateinit var pieChart: PieChart
     private val calendar = Calendar.getInstance()
+    lateinit var barChart: BarChart
+    lateinit var barData: BarData
+    lateinit var barDataSet: BarDataSet
+    lateinit var barEntriesList: ArrayList<BarEntry>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -111,95 +114,159 @@ class WeekFragment : Fragment() {
                 }
                 if (isAdded)// This {@link androidx.fragment.app.Fragment} class method is responsible to check if the your view is attached to the Activity or not
                 {
-                    pieChart = binding.pieChart
-//                    getDataFromFireBase(todaysDate)
-                    // on below line we are setting user percent value,
-                    // setting description as enabled and offset for pie chart
-                    pieChart.setUsePercentValues(true)
-                    pieChart.description.isEnabled = false
-                    pieChart.setExtraOffsets(5f, 10f, 5f, 5f)
+                    showPieChart(fiber, protein, fat, calories, cholestrol)
 
-                    // on below line we are setting drag for our pie chart
-                    pieChart.dragDecelerationFrictionCoef = 0.95f
+                    showBarChart(fiber, protein, fat, calories, cholestrol)
 
-                    // on below line we are setting hole
-                    // and hole color for pie chart
-                    pieChart.isDrawHoleEnabled = true
-                    pieChart.setHoleColor(Color.WHITE)
-
-                    // on below line we are setting circle color and alpha
-                    pieChart.setTransparentCircleColor(Color.WHITE)
-                    pieChart.setTransparentCircleAlpha(110)
-
-                    // on  below line we are setting hole radius
-                    pieChart.holeRadius = 58f
-                    pieChart.transparentCircleRadius = 61f
-
-                    // on below line we are setting center text
-                    pieChart.setDrawCenterText(true)
-
-                    // on below line we are setting
-                    // rotation for our pie chart
-                    pieChart.rotationAngle = 0f
-
-                    // enable rotation of the pieChart by touch
-                    pieChart.isRotationEnabled = true
-                    pieChart.isHighlightPerTapEnabled = true
-
-                    // on below line we are setting animation for our pie chart
-//                    pieChart.animateY(1400, Easing.EaseInOutQuad)
-
-                    // on below line we are disabling our legend for pie chart
-                    pieChart.legend.isEnabled = false
-                    pieChart.setEntryLabelColor(Color.WHITE)
-                    pieChart.setEntryLabelTextSize(12f)
-
-                    pieChart.animateY(1400, Easing.EaseInOutQuad)
-                    val entries: ArrayList<PieEntry> = ArrayList()
-                    entries.add(PieEntry(fiber.toFloat()))
-                    entries.add(PieEntry(protein.toFloat()))
-                    entries.add(PieEntry(fat.toFloat()))
-                    entries.add(PieEntry(calories.toFloat()))
-                    entries.add(PieEntry(cholestrol.toFloat()))
-
-                    val dataSet = PieDataSet(entries, "DAILY Intake")
-                    dataSet.setDrawIcons(false)
-
-                    // on below line we are setting slice for pie
-                    dataSet.sliceSpace = 6f
-                    dataSet.iconsOffset = MPPointF(0f, 40f)
-                    dataSet.selectionShift = 10f
-
-                    // add a lot of colors to list
-                    val colors: ArrayList<Int> = ArrayList()
-                    colors.add(resources.getColor(R.color.teal_200))
-                    colors.add(resources.getColor(R.color.yellow))
-                    colors.add(resources.getColor(R.color.red))
-                    colors.add(resources.getColor(R.color.material_dynamic_primary70))
-                    colors.add(resources.getColor(R.color.green_c))
-
-                    // on below line we are setting colors.
-                    dataSet.colors = colors
-
-                    // on below line we are setting pie data set
-                    val data = PieData(dataSet)
-                    data.setValueFormatter(PercentFormatter())
-                    data.setValueTextSize(15f)
-                    data.setValueTypeface(Typeface.DEFAULT_BOLD)
-                    data.setValueTextColor(Color.WHITE)
-                    pieChart.data = data
-
-                    // undo all highlights
-                    pieChart.highlightValues(null)
-
-                    // loading chart
-                    pieChart.invalidate()
                 }
             }
 
             override fun onCancelled(error: DatabaseError) {
             }
         })
+    }
+
+    private fun showBarChart(
+        fiber: Int,
+        protein: Int,
+        fat: Int,
+        calories: Int,
+        cholestrol: Int
+    ) {
+        barChart = binding.idBarChart
+        // on below line we are calling get bar
+        // chart data to add data to our array list
+        barEntriesList = ArrayList()
+
+        // on below line we are adding data
+        // to our bar entries list
+        barEntriesList.add(BarEntry(1f, fiber.toFloat()))
+        barEntriesList.add(BarEntry(2f, protein.toFloat()))
+        barEntriesList.add(BarEntry(3f, fat.toFloat()))
+        barEntriesList.add(BarEntry(4f, calories.toFloat()))
+        barEntriesList.add(BarEntry(5f, cholestrol.toFloat()))
+
+        // on below line we are initializing our bar data set
+        barDataSet = BarDataSet(barEntriesList, "Bar Chart Data")
+
+        // on below line we are initializing our bar data
+        barData = BarData(barDataSet)
+
+        // on below line we are setting data to our bar chart
+        barChart.data = barData
+
+        // on below line we are setting colors for our bar chart text
+        barDataSet.valueTextColor = Color.BLACK
+
+        // on below line we are setting color for our bar data set
+        barDataSet.color = resources.getColor(R.color.red)
+        //                    barDataSet.setColors(ColorTemplate.JOYFUL_COLORS)
+
+        // on below line we are setting text size
+        barDataSet.valueTextSize = 16f
+        barChart.xAxis.setDrawGridLines(false)
+        barChart.axisLeft.setDrawGridLines(false)
+        barChart.axisRight.setDrawGridLines(false)
+//        barChart.axisRight.isEnabled = false;
+
+
+        // on below line we are enabling description as false
+        barChart.description.isEnabled = false
+        barChart.invalidate()
+        barChart.refreshDrawableState()
+    }
+
+
+    private fun showPieChart(
+        fiber: Int,
+        protein: Int,
+        fat: Int,
+        calories: Int,
+        cholestrol: Int
+    ) {
+        pieChart = binding.pieChart
+        //                    getDataFromFireBase(todaysDate)
+        // on below line we are setting user percent value,
+        // setting description as enabled and offset for pie chart
+        pieChart.setUsePercentValues(true)
+        pieChart.description.isEnabled = false
+        pieChart.setExtraOffsets(5f, 10f, 5f, 5f)
+
+        // on below line we are setting drag for our pie chart
+        pieChart.dragDecelerationFrictionCoef = 0.95f
+
+        // on below line we are setting hole
+        // and hole color for pie chart
+        pieChart.isDrawHoleEnabled = true
+        pieChart.setHoleColor(Color.WHITE)
+
+        // on below line we are setting circle color and alpha
+        pieChart.setTransparentCircleColor(Color.WHITE)
+        pieChart.setTransparentCircleAlpha(110)
+
+        // on  below line we are setting hole radius
+        pieChart.holeRadius = 58f
+        pieChart.transparentCircleRadius = 61f
+
+        // on below line we are setting center text
+        pieChart.setDrawCenterText(true)
+
+        // on below line we are setting
+        // rotation for our pie chart
+        pieChart.rotationAngle = 0f
+
+        // enable rotation of the pieChart by touch
+        pieChart.isRotationEnabled = true
+        pieChart.isHighlightPerTapEnabled = true
+
+        // on below line we are setting animation for our pie chart
+        //                    pieChart.animateY(1400, Easing.EaseInOutQuad)
+
+        // on below line we are disabling our legend for pie chart
+        pieChart.legend.isEnabled = false
+        pieChart.setEntryLabelColor(Color.WHITE)
+        pieChart.setEntryLabelTextSize(12f)
+
+        pieChart.animateY(1400, Easing.EaseInOutQuad)
+        val entries: ArrayList<PieEntry> = ArrayList()
+        entries.add(PieEntry(fiber.toFloat()))
+        entries.add(PieEntry(protein.toFloat()))
+        entries.add(PieEntry(fat.toFloat()))
+        entries.add(PieEntry(calories.toFloat()))
+        entries.add(PieEntry(cholestrol.toFloat()))
+
+        val dataSet = PieDataSet(entries, "DAILY Intake")
+        dataSet.setDrawIcons(false)
+
+        // on below line we are setting slice for pie
+        dataSet.sliceSpace = 6f
+        dataSet.iconsOffset = MPPointF(0f, 40f)
+        dataSet.selectionShift = 10f
+
+        // add a lot of colors to list
+        val colors: ArrayList<Int> = ArrayList()
+        colors.add(resources.getColor(R.color.teal_200))
+        colors.add(resources.getColor(R.color.yellow))
+        colors.add(resources.getColor(R.color.red))
+        colors.add(resources.getColor(R.color.material_dynamic_primary70))
+        colors.add(resources.getColor(R.color.green_c))
+
+        // on below line we are setting colors.
+        dataSet.colors = colors
+
+        // on below line we are setting pie data set
+        val data = PieData(dataSet)
+        data.setValueFormatter(PercentFormatter())
+        data.setValueTextSize(15f)
+        data.setValueTypeface(Typeface.DEFAULT_BOLD)
+        data.setValueTextColor(Color.WHITE)
+        pieChart.data = data
+
+        // undo all highlights
+        pieChart.highlightValues(null)
+
+        // loading chart
+        pieChart.invalidate()
     }
 
 }
